db0a58eed13baccc4c23f6591f9ba243bf53fb67
necosmos <47885627+necosmos@users.noreply.github.com>
2021-02-25 13:23:54 +0200

    Lab_1.1

---
+++ Armageddon3000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Armageddon3000
+//
+//  Created by Пінчук Микита on 25.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
